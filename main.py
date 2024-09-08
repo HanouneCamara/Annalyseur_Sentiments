4f56eac0d8afbb4bc63aea1dd703343b169f329f
HanouneCamara <Alicmr13@gmail.com>
2024-09-08 13:49:27 +0000

    traducteurFrEn

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from textblob import TextBlob
-import nltk
+from googletrans import Translator
 
-nltk.download('punkt_tab')
-
+# Initialiser le traducteur
+translator = Translator()
 
 # Boucle pour demander une saisie utilisateur
 while True:
@@ -11,16 +11,23 @@ while True:
     if texte.lower() == 'exit':
         print("Au revoir!")
         break
-    # Créer un objet TextBlob avec le texte saisi
-    blob = TextBlob(texte)
     
-    # Analyse de sentiment
-    sentiment = blob.sentiment.polarity  # Donne un score de -1 (négatif) à 1 (positif)
- 
-     # Affichage du résultat
-    if sentiment > 0:
-        print("Sentiment positif")
-    elif sentiment < 0:
-        print("Sentiment négatif")
-    else:
-        print("Sentiment neutre")
+    try:
+        # Traduire le texte en anglais
+        traduction = translator.translate(texte, dest='en')
+        texte_traduit = traduction.text
+
+        # Analyse de sentiment sur le texte traduit
+        blob = TextBlob(texte_traduit)
+        sentiment = blob.sentiment.polarity  # Donne un score de -1 (négatif) à 1 (positif)
+
+        # Affichage du résultat
+        #print(f"Texte traduit: {texte_traduit}")
+        if sentiment > 0:
+            print("Sentiment positif")
+        elif sentiment < 0:
+            print("Sentiment négatif")
+        else:
+            print("Sentiment neutre")
+    except Exception as e:
+        print(f"Erreur lors de la traduction ou de l'analyse : {e}")
